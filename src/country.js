da6a08c5775127fa3ab5fb26573d39d56e0539df
ikudria <irakudrya@gmail.com>
2022-07-11 08:39:50 -0500

    Task-01-completed

--- src/country.js
+++ src/country.js
@@ -1,11 +1,13 @@
 'use strict';
+import Notiflix from 'notiflix';
 
 const BASE_URL = 'https://restcountries.com/v3.1';
-const filter = '?fields=name,capital,population,languages';
+const filter = '?fields=name,capital,population,languages,flags';
 
-const fetchCountry = country => {
-  return fetch(BASE_URL + '/name/' + reqCountry + filter).then(response => {
+export const fetchCountry = country => {
+  return fetch(BASE_URL + '/name/' + country + filter).then(response => {
     if (!response.ok) {
+      console.log('error');
       throw Notiflix.Notify.failure('Oops, there is no country with that name');
     }
     return response.json();
--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import debounce from 'lodash.debounce';
 import './css/styles.css';
 import countriesList from './templates/countries-list.hbs';
 import countryCard from './templates/country-card.hbs';
-console.log(countriesList());
+import { fetchCountry } from './country';
 
 const DEBOUNCE_DELAY = 300;
-
-// start of my script
-const BASE_URL = 'https://restcountries.com/v3.1';
 
 const infoContainer = document.querySelector('.country-info');
 const list = document.querySelector('.country-list');
@@ -17,36 +14,33 @@ searchBox.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
 
 function onSearchInput(e) {
   const reqCountry = e.target.value.trim();
-  console.log(reqCountry);
 
-  const filter = '?fields=name,capital,population,languages,flags';
+  if (reqCountry === '') {
+    list.innerHTML = '';
+    infoContainer.innerHTML = '';
+    return;
+  }
 
-  fetch(BASE_URL + '/name/' + reqCountry + filter)
-    .then(response => {
-      if (!response.ok) {
-        throw Notiflix.Notify.failure(
-          'Oops, there is no country with that name'
-        );
-      }
-      return response.json();
-    })
+  fetchCountry(reqCountry)
     .then(countries => {
       if (countries.length > 10) {
-        return Notiflix.Notify.failure(
+        return Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
       } else if (countries.length >= 2 && countries.length <= 10) {
         console.log(countries);
         const countryList = countries.map(country => countriesList(country));
         list.innerHTML = countryList.join('');
+        infoContainer.innerHTML = '';
+
         console.log(countryList);
-        return;
       } else if (countries.length === 1) {
         console.log(countries);
 
         const info = countries.map(country => countryCard(country));
 
         infoContainer.innerHTML = info;
+        list.innerHTML = '';
       }
     })
     .catch(error => console.log(error));
